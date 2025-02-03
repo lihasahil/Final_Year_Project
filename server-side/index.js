ae292059440b3619742728078107e576f6969b62
Sahil Shrestha <137997098+lihasahil@users.noreply.github.com>
2025-02-03 18:02:33 +0545

    Update index.js

--- server-side/index.js
+++ server-side/index.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: "https://final-year-project-1-e3pr.onrender.com" }));
 // database connection
 connectDB();
 // register routes
