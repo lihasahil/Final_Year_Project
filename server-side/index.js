1d4066734cfca07c94482e66dcd88424f3dcf9d0
lihasahil <sahilshrestha106@gmail.com>
2025-02-04 16:27:27 +0545

    deployment

--- server-side/index.js
+++ server-side/index.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 9051;
 
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: "findmefrontend.vercel.app" }));
 // database connection
 connectDB();
 // register routes
