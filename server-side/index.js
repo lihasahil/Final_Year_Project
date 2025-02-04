1e27e365dadcbb7e5c61263e54a42c3e3ac9c9a6
lihasahil <sahilshrestha106@gmail.com>
2025-02-04 16:44:12 +0545

    commit

--- server-side/index.js
+++ server-side/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 9051;
 app.use(express.json());
 app.use(
   cors({
-    origin: "findmefrontend.vercel.app",
+    origin: ["findmefrontend.vercel.app"],
     methods: ["POST", "GET"],
     credentials: true,
   })
