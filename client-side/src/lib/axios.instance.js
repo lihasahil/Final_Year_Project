59b1ecb4375ed70e9a58fa4ef2ba35c866a70a92
lihasahil <sahilshrestha106@gmail.com>
2025-02-04 16:23:53 +0545

    deployment

--- client-side/src/lib/axios.instance.js
+++ client-side/src/lib/axios.instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:9051",
+  baseURL: "findmebackend-iota.vercel.app",
 });
 
 axiosInstance.interceptors.request.use(function (config) {
